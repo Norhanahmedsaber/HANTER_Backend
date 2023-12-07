cda154d51d6be423c773c5d4e5bf4ddf7947cb5e
2152001 <tokaabdelgawad60@gmail.com>
2023-12-07 16:55:41 +0200

    auth update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,7 +6,12 @@ const auth = async (req , res , next)=>{
     try {
         const token = req.header('Authorization').replace('Bearer ' ,'')
         const decoded = jwt.verify(token , process.env.SECRET)
-        
+        //replace signin with getbyID
+        const user = await User.signIn()
+        if(!user) throw new Error()
+        req.user = user
+        req.token = token
+        next()
     }catch(error)
     {
         res.status(401).send({error:"user not authorized"})
