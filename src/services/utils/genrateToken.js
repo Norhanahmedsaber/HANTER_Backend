759675739528e294c8134e4ed0215890e016b012
2152001 <tokaabdelgawad60@gmail.com>
2023-12-10 11:58:48 +0200

    toka

--- src/services/utils/genrateToken.js
+++ src/services/utils/genrateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 require('dotenv').config()
 
 function generateToken(user) {
-    const token = jwt.sign({id:user.id , email:user.email} ,process.env.SECRET)
+    const token = jwt.sign({id:user.id , email:user.email} ,process.env.SECRET , {expiresIn:'24h'})
     user.token = token
 }
 
