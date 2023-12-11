1ced587b2aefda39afbed3bd65b49b60956f1bc2
anas12312 <anashesham000@gmail.com>
2023-12-11 09:45:31 +0200

    fixing cors error

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
 const cors = require('cors')
 
-app.use(cors())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
