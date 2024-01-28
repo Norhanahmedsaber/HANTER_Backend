9f049c9562dbdaa80c3803c07cc2167b325f2928
2152001 <tokaabdelgawad60@gmail.com>
2024-01-28 16:43:10 +0200

    toka

--- src/routers/rule.js
+++ src/routers/rule.js
@@ -1,110 +1,123 @@
-const express = require('express')
-const router=new express.Router()
-const auth =require ('../middlewares/auth')
-const uploadRule =require('../middlewares/upload')
-const ruleServices = require('../services/rule')
-const ftp = require('basic-ftp')
-const {Readable} = require('stream')
+const express = require("express");
+const router = new express.Router();
+const auth = require("../middlewares/auth");
+const uploadRule = require("../middlewares/upload");
+const ruleServices = require("../services/rule");
+const ftp = require("basic-ftp");
+const { Readable } = require("stream");
 
 // create rule (uploaded file)
-router.post('/rules', auth, async(req, res) => {
-    const ruleName = req.body.name
-    const createdBy = req.user.id
-    if(!req.files) {
-        return res.status(400).send({
-            message: "No File Uploaded"
-        })
+router.post("/rules", auth, async (req, res) => {
+  const ruleName = req.body.name;
+  const createdBy = req.user.id;
+  const public = req.body.public;
+  const severity = req.body.severity;
+  if (!req.files) {
+    return res.status(400).send({
+      message: "No File Uploaded",
+    });
+  }
+  if (!req.files.rule) {
+    return res.status(400).send({
+      message: "No File Uploaded",
+    });
+  }
+  const rule = req.files.rule;
+  try {
+    const result = await ruleServices.addRule(
+      rule,
+      ruleName,
+      createdBy,
+      public,
+      severity
+    );
+    if (result.message) {
+      return res.status(result.statusCode).send({
+        message: result.message,
+      });
     }
-    if(!req.files.rule) {
-        return res.status(400).send({
-            message: "No File Uploaded"
-        })
-    }
-    const rule = req.files.rule
-    try {
-        const result = await ruleServices.addRule(rule, ruleName, createdBy)
-        if(result.message) {
-            return res.status(result.statusCode).send({
-                message: result.message
-            })
-        }
-        res.send(result)
-    }catch (e) {
-        console.log(e)
-        res.status(500).send({
-            message: "Internal Server Error, Please Try Again Later"
-        })
-    }
-})
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({
+      message: "Internal Server Error, Please Try Again Later",
+    });
+  }
+});
 //create rule with string
-router.post('/rule' ,auth , async(req ,res)=>{
-    const ruleName = req.body.name
-    const createdBy = req.user.id
-    const content = req.body.content
-    try{
-        const result = await ruleServices.addRuleString(ruleName , createdBy , content)
-        if(result.message){
-            return res.status(result.statusCode).send({
-                message: result.message
-            })
-        }
-        res.send(result)
-    }catch(e){
-        console.log(e)
-        res.status(500).send({
-            message: "Internal Server Error, Please Try Again Later"
-        })
+router.post("/rule", auth, async (req, res) => {
+  const ruleName = req.body.name;
+  const createdBy = req.user.id;
+  const content = req.body.content;
+  const public = req.body.public;
+  const severity = req.body.severity;
+  try {
+    const result = await ruleServices.addRuleString(
+      ruleName,
+      createdBy,
+      content,
+      public,
+      severity
+    );
+    if (result.message) {
+      return res.status(result.statusCode).send({
+        message: result.message,
+      });
     }
+    res.send(result);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({
+      message: "Internal Server Error, Please Try Again Later",
+    });
+  }
+});
 
-})
-
-router.delete('/rules/:uuid', auth, async (req, res) => {
-    const uuid = req.params.uuid
-    const userId = req.user.id
-    const result=await ruleServices.deleteRule(uuid, userId)
-    if(result.message) {
-        return res.status(result.statusCode).send({
-            message: result.message
-        })
-    }
-    res.send({
-        message: "Deleted Successfully"
-    })
-})
+router.delete("/rules/:uuid", auth, async (req, res) => {
+  const uuid = req.params.uuid;
+  const userId = req.user.id;
+  const result = await ruleServices.deleteRule(uuid, userId);
+  if (result.message) {
+    return res.status(result.statusCode).send({
+      message: result.message,
+    });
+  }
+  res.send({
+    message: "Deleted Successfully",
+  });
+});
 // Get user rules
-router.get('/rules',auth,async (req,res) => {
-    const id = req.user.id
-    const result = await ruleServices.getUserRules(id)
-    if(result.message) {
-        return res.status(result.statusCode).send({
-            message: result.message
-        })
-    }
-    res.send(result)
-})
+router.get("/rules", auth, async (req, res) => {
+  const id = req.user.id;
+  const result = await ruleServices.getUserRules(id);
+  if (result.message) {
+    return res.status(result.statusCode).send({
+      message: result.message,
+    });
+  }
+  res.send(result);
+});
 
-router.get('/rules/:id' ,auth, async(req ,res)=>{
-    const id = req.params.id
-    const userId=req.user.id
-    const result = await ruleServices.getCustomRule(id,userId)
-    if(result.message){
-        return res.status(result.statusCode).send({
-            message:result.message
-        })
-    }
-    res.send(result)
+router.get("/rules/:id", auth, async (req, res) => {
+  const id = req.params.id;
+  const userId = req.user.id;
+  const result = await ruleServices.getCustomRule(id, userId);
+  if (result.message) {
+    return res.status(result.statusCode).send({
+      message: result.message,
+    });
+  }
+  res.send(result);
+});
+router.get("/system", async (req, res) => {
+  try {
+    const rules = await ruleServices.getSystemRules();
+    res.send(rules);
+  } catch (e) {
+    res.status(500).send({
+      message: "Internal Server Error",
+    });
+  }
+});
 
-})
-router.get('/system', async (req, res) => {
-
-    try {
-        const rules = await ruleServices.getSystemRules()
-        res.send(rules)
-    }catch(e) {
-        res.status(500).send({
-            message: "Internal Server Error"
-        })
-    }
-})
-
-module.exports = router
+module.exports = router;
